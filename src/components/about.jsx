6fc5c697cf8f7c9174d45ef63ce65683c698d2b0
Tanyashree-M <tanyashree2006@gmail.com>
2024-09-13 19:59:52 +0530

    fix(about us): enable carousel

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -18,43 +18,73 @@ export const About = () => {
   }
 
   return (
-    <div id="about">
-      <div className="container">
-        <div className="row">
-          <div className="col-xs-12 col-md-6">
-            {" "}
-            <img src="img/about/about.jpg" className="img-responsive" alt="" />{" "}
+    <div>
+      <div id="myCarousel" className="carousel slide" data-bs-ride="carousel" interval="1500" >
+
+        <ol className="carousel-indicators">
+          <li data-target="#myCarousel" data-slide-to="0" className="active"></li>
+          <li data-target="#myCarousel" data-slide-to="1"></li>
+          <li data-target="#myCarousel" data-slide-to="2"></li>
+          <li data-target="#myCarousel" data-slide-to="3"></li>
+        </ol>
+
+        <div className="carousel-inner" style={{
+          "display": "flex", "justify-content": "center",
+          "height": "100vh"
+        }}>
+
+          <div className="item active" >
+            <img src="./img/about/about1.png" alt="about images" />
           </div>
-          <div className="col-xs-12 col-md-6">
-            <div className="about-text">
-              <h2>About Us</h2>
-              <p>{about ? about.paragraph : "loading..."}</p>
-              <h3>Why Choose Us?</h3>
-              <div className="list-style">
-                <div className="col-lg-6 col-sm-6 col-xs-12">
-                  <ul>
-                    {about
-                      ? about.Why.map((d, i) => (
-                        <li key={`${d}-${i}`}>{d}</li>
-                      ))
-                      : "loading"}
-                  </ul>
-                </div>
-                <div className="col-lg-6 col-sm-6 col-xs-12">
-                  <ul>
-                    {about
-                      ? about.Why2.map((d, i) => (
-                        <li key={`${d}-${i}`}> {d}</li>
-                      ))
-                      : "loading"}
-                  </ul>
-                </div>
-              </div>
-            </div>
+
+          <div className="item" >
+            <img src="./img/about/about2.png" alt="about images" />
           </div>
+
+          <div className="item" >
+            <img src="./img/about/about3.png" alt="about images" />
+          </div>
+
+          <div className="item" >
+            <img src="./img/about/about4.png" alt="about images" />
+
+          </div>
+
+        </div>
+        <a className="left carousel-control" href="#myCarousel" data-slide="prev" >
+          <i className="fa fa-chevron-left" style={{ "position": "absolute", 'top': '50%', 'color': 'black' }}></i>
+          <span className="sr-only">Previous</span>
+        </a>
+        <a className="right carousel-control" href="#myCarousel" data-slide="next">
+          <i className="fa fa-chevron-right" style={{ "position": "absolute", 'top': '50%', 'color': 'black' }}></i>
+          <span className="sr-only">Next</span>
+        </a>
+      </div>
+      <h2 style={{ "margin-top": "20px" }}>About Us</h2>
+      <p>{about ? about.paragraph : "loading..."}</p>
+      <h3>Why Choose Us?</h3>
+      <div className="list-style">
+        <div className="col-lg-6 col-sm-6 col-xs-12">
+          <ul>
+            {about
+              ? about.Why.map((d, i) => (
+                <li key={`${d}-${i}`}>{d}</li>
+              ))
+              : "loading"}
+          </ul>
+        </div>
+        <div className="col-lg-6 col-sm-6 col-xs-12">
+          <ul>
+            {about
+              ? about.Why2.map((d, i) => (
+                <li key={`${d}-${i}`}> {d}</li>
+              ))
+              : "loading"}
+          </ul>
         </div>
       </div>
-    </div>
+    </div >
+
   );
 };
 
