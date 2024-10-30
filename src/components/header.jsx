cdb45d45a2d7f03ac202bd1497ae109fd3f4a9a0
Tanyashree-M <tanyashree2006@gmail.com>
2024-10-30 19:15:10 +0530

    fix(home): Fixed alignment

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -5,26 +5,25 @@ export const Header = () => {
     return (
         <header id="header">
             <div className="intro">
-                <div className="overlay">
-                    <div className="container">
-                        <div className="row">
-                            <div className="col-md-8 col-md-offset-2 intro-text">
-                                <h1>
-                                    Welcome TO JEYAKAR.
-                                    <span></span>
-                                </h1>
-                                <p>High Quality printing and binding services</p>
-                                <br></br>
-                                <p>Jeyakar offset(p) ltd is one of the quality manufacturers of printing services in sivakasi.our team works with passionate hearts and tireless hands to serve our customers which include designing, printing, packing, binding and finishing. </p>
-                                <NavLink
-                                    to="services"
-                                    className="btn btn-custom btn-lg page-scroll"
-                                >
-                                    Learn More
-                                </NavLink>{" "}
-                            </div>
+                <div className="container" style={{ 'paddingTop': '100px' }}>
+                    <div className="row" style={{ "backgroundColor": "#000", "opacity": "0.5", 'borderRadius': '40px' }}>
+                        <div className="col-md-8 col-md-offset-2 intro-text"  >
+                            <h1>
+                                Welcome TO JEYAKAR.
+                                <span></span>
+                            </h1>
+                            <p>High Quality printing and binding services</p>
+                            <br></br>
+                            <p>Jeyakar offset(p) ltd is one of the quality manufacturers of printing services in Sivakasi.our team works with passionate hearts and tireless hands to serve our customers which include designing, printing, packing, binding and finishing. </p>
+
                         </div>
                     </div>
+                    <NavLink
+                        to="services"
+                        className="btn btn-custom btn-lg page-scroll"
+                    >
+                        Learn More
+                    </NavLink>{" "}
                 </div>
             </div>
         </header>
