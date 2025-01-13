e6633c2c5559b76edb5cca3eed882a42b0acf97f
Tanyashree-M <tanyashree2006@gmail.com>
2025-01-13 11:24:07 +0530

    fix(products): Added product images

--- src/components/products.jsx
+++ src/components/products.jsx
@@ -4,49 +4,49 @@ import React from "react";
 export const Products = () => {
   const products = [
     {
-      "title": "Product Name",
-      "largeImage": "img/portfolio/01-large.jpg",
-      "smallImage": "img/portfolio/01-small.jpg"
+      "title": "Calendars",
+      "largeImage": "img/products/p1-large.jpg",
+      "smallImage": "img/products/p1-small.jpg"
     },
     {
-      "title": "Product Name",
-      "largeImage": "img/portfolio/02-large.jpg",
-      "smallImage": "img/portfolio/02-small.jpg"
+      "title": "Personalised Notebooks",
+      "largeImage": "img/products/p2-large.jpg",
+      "smallImage": "img/products/p2-small.jpg"
     },
     {
-      "title": "Product Name",
-      "largeImage": "img/portfolio/03-large.jpg",
-      "smallImage": "img/portfolio/03-small.jpg"
+      "title": "Personalised Diaries",
+      "largeImage": "img/products/p3-large.jpg",
+      "smallImage": "img/products/p3-small.jpg"
     },
     {
-      "title": "Product Name",
-      "largeImage": "img/portfolio/04-large.jpg",
-      "smallImage": "img/portfolio/04-small.jpg"
+      "title": "Personalised Diaries",
+      "largeImage": "img/products/p4-large.jpg",
+      "smallImage": "img/products/p4-small.jpg"
     },
     {
-      "title": "Product Name",
-      "largeImage": "img/portfolio/05-large.jpg",
-      "smallImage": "img/portfolio/05-small.jpg"
+      "title": "Personalised Notebooks",
+      "largeImage": "img/products/p5-large.jpg",
+      "smallImage": "img/products/p5-small.jpg"
     },
     {
-      "title": "Product Name",
-      "largeImage": "img/portfolio/06-large.jpg",
-      "smallImage": "img/portfolio/06-small.jpg"
+      "title": "Personalised Diaries",
+      "largeImage": "img/products/p6-large.jpg",
+      "smallImage": "img/products/p6-small.jpg"
     },
     {
-      "title": "Product Name",
-      "largeImage": "img/portfolio/07-large.jpg",
-      "smallImage": "img/portfolio/07-small.jpg"
+      "title": "Diaries",
+      "largeImage": "img/products/p7-large.jpg",
+      "smallImage": "img/products/p7-small.jpg"
     },
     {
-      "title": "Product Name",
-      "largeImage": "img/portfolio/08-large.jpg",
-      "smallImage": "img/portfolio/08-small.jpg"
+      "title": "Notebooks",
+      "largeImage": "img/products/p8-large.jpg",
+      "smallImage": "img/products/p8-small.jpg"
     },
     {
-      "title": "Product Name",
-      "largeImage": "img/portfolio/09-large.jpg",
-      "smallImage": "img/portfolio/09-small.jpg"
+      "title": "Notebooks",
+      "largeImage": "img/products/p9-large.jpg",
+      "smallImage": "img/products/p9-small.jpg"
     }
   ]
   return (
@@ -55,8 +55,26 @@ export const Products = () => {
         <div className="section-title">
           <h2>Products</h2>
         </div>
+        <div class="row">
+          <div class="col-12 col-md-4" style={{ 'padding': '10px' }}>
+            <div class="embed-responsive embed-responsive-16by9">
+              <video controls autoplay loop src="img/products/v1.mp4" class="embed-responsive-item"></video>
+            </div>
+          </div>
+          <div class="col-12 col-md-4" style={{ 'padding': '10px' }}>
+            <div class="embed-responsive embed-responsive-16by9">
+              <video controls autoplay loop src="img/products/v2.mp4" class="embed-responsive-item"></video>
+            </div>
+          </div>
+          <div class="col-12 col-md-4" style={{ 'padding': '10px' }}>
+            <div class="embed-responsive embed-responsive-16by9">
+              <video controls autoplay loop src="img/products/v3.mp4" class="embed-responsive-item"></video>
+            </div>
+          </div>
+        </div>
+
         <div className="row">
-          <div className="portfolio-items">
+          <div className="products-items">
             {products
               ? products.map((d, i) => (
                 <div
